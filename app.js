b1cf2d4b4f0475bfb7101c17091e967db60e9536
RitikWeb22 <bussinessgrow1@gmail.com>
2025-06-15 12:42:27 +0530

    demo

--- app.js
+++ app.js
@@ -10,29 +10,39 @@ app.use(express.static(path.join(__dirname, "public")));
 const server = http.createServer(app);
 const io = socketio(server);
 
-// Store users
+// Store users and their last location
 const users = {};
 
 io.on("connection", (socket) => {
     console.log("User connected:", socket.id);
 
-    // Get username from client
-    socket.on("register-user", (username) => {
-        users[socket.id] = { username };
-    });
+    // Assign random username and store
+    const username = "User-" + Math.floor(Math.random() * 1000);
+    users[socket.id] = { username };
 
-    socket.on("send-location", (data) => {
+    // Send all existing users' location to the newly connected client
+    const existingUsers = [];
+    for (let id in users) {
+        if (users[id].location && id !== socket.id) {
+            existingUsers.push({ id, username: users[id].username, ...users[id].location });
+        }
+    }
+    socket.emit("existing-users", existingUsers);
+
+    // When user sends location
+    socket.on("send-location", (location) => {
+        users[socket.id].location = location;
         io.emit("receive-location", {
             id: socket.id,
-            username: users[socket.id]?.username || "Anonymous",
-            ...data,
+            username: users[socket.id].username,
+            ...location,
         });
     });
 
     socket.on("disconnect", () => {
-        io.emit("user-disconnected", socket.id);
-        delete users[socket.id];
         console.log("User disconnected:", socket.id);
+        delete users[socket.id];
+        io.emit("user-disconnected", socket.id);
     });
 });
 
--- public/js/script.js
+++ public/js/script.js
@@ -1,76 +1,38 @@
 const socket = io();
-
-// Generate a simple random username (no prompt)
-const username = "User-" + Math.floor(Math.random() * 1000);
-socket.emit("register-user", username);
-
-const map = L.map("map").setView([0, 0], 16);
-L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-    attribution: "Ritik",
-}).addTo(map);
-
-const markers = {};
-const colors = {};
-const getRandomColor = () => "#" + Math.floor(Math.random() * 16777215).toString(16);
-
-// Store your own socket id
-let myId = null;
-
-socket.on("connect", () => {
-    myId = socket.id;
-});
-
-// Track and send location
 if (navigator.geolocation) {
-    navigator.geolocation.watchPosition(
-        (position) => {
-            const { latitude, longitude } = position.coords;
-            socket.emit("send-location", { latitude, longitude });
+    navigator.geolocation.watchPosition((position) => {
+        const { latitude, longitude } = position.coords;
+        socket.emit("send-location", { latitude, longitude });
 
-            // Center map only on YOUR location updates
-            if (myId) {
-                // We can be sure this is your own update
-                map.setView([latitude, longitude], 16);
-            }
-        },
-        (error) => console.log(error),
-        {
-            enableHighAccuracy: true,
-            timeout: 5000,
-            maximumAge: 0,
-        }
+
+    }, (error) => {
+        console.log(error);
+    }, {
+        enableHighAccuracy: true,
+        timeout: 5000,
+        maximumAge: 0,
+    }
     );
 }
+const map = L.map("map").setView([0, 0], 16);
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    attribution: "Ritik"
+}).addTo(map);
+const markers = {};
 
-// Receive location of all users
 socket.on("receive-location", (data) => {
-    const { id, latitude, longitude, username } = data;
-
-    if (!colors[id]) {
-        colors[id] = getRandomColor();
-    }
-
+    const { id, latitude, longitude } = data;
+    map.setView([latitude, longitude]);
     if (markers[id]) {
         markers[id].setLatLng([latitude, longitude]);
+
     } else {
-        const marker = L.circleMarker([latitude, longitude], {
-            radius: 8,
-            color: colors[id],
-            fillColor: colors[id],
-            fillOpacity: 0.9,
-        }).addTo(map);
-
-        // Remove username label if you want:
-        // marker.bindTooltip(username, { permanent: true, direction: "top", className: "user-label" });
-
-        markers[id] = marker;
+        markers[id] = L.marker([latitude, longitude]).addTo(map);
     }
 });
-
-socket.on("user-disconnected", (id) => {
+socket.on("user-disconnected", () => {
     if (markers[id]) {
         map.removeLayer(markers[id]);
         delete markers[id];
-        delete colors[id];
     }
 });
